df2a6d688129a2e1163dcd150c822280154c09b4
Somnath Chakraborty <somnathchakraborty.mail@gmail.com>
2026-06-06 02:40:38 +0530

    Remove W&B entity, use default account

--- src/train.py
+++ src/train.py
@@ -61,7 +61,6 @@ def compute_metrics(pred):
 def train_version(train_dataset, test_dataset, model, tokenizer, version_config):
     wandb.init(
         project="mlops-group34",
-        entity="somnath_chakraborty-indian-institute-of-technology-jodhpur",
         name=f"run-{version_config['version']}",
         config={
             "model": MODEL_NAME,
